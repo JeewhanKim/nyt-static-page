a9baffa02a4e7ec41097f29ef75cc80780d7c31d
Jeewhan Kim <awryong@gmail.com>
2018-04-30 14:32:39 -0400

    initial commit - folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,63 @@
 
 const gulp = require('gulp')
-// const webpack = require('webpack-stream')
+const modRewrite = require('connect-modrewrite')
+const browserSync = require('browser-sync')
+const gulpLoadPlugins = require('gulp-load-plugins')
+const plugins = gulpLoadPlugins()
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
 
 gulp.task('default', () =>  {
-  gulp.start('dev');
-});
-
-gulp.task('dev', () => {
-//   return gulp.start('build-react')
+  gulp.start('dev')
 })
 
-// gulp.task('build-html', () => {
-//   return gulp.src('./src/view/*.html')
-//   .pipe(gulp.dest('./dist/'))
-// })
+gulp.task('dev', [
+  'dev-sass',
+  'dev-js',
+  'dev-views',
+], () => {gulp.start('dev-browserSync')})
 
-// gulp.task('build-react', () => {
-//   return gulp.src('./src/index.jsx')
-//   .pipe(webpack(require('./webpack.config.js')))
-//   .pipe(gulp.dest('./dist/js/'))
-// })
+gulp.task('dev-sass', () => {
+  return gulp.src('./src/style/index.scss')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({
+      indentedSyntax: false,
+      imagePath: 'images'
+    }))
+    .pipe(plugins.concat('style.css'))
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({stream:true}));
+})
 
-// gulp.task('prod', () => {
-//   return gulp.src('./src/index.jsx')
-//   .pipe(webpack(require('./webpack.prod.config.js')))
-//   .pipe(gulp.dest('./dist/js/'))
-// })
+gulp.task('dev-js', () => {
+  return browserify({entries: ['./src/js/index.js']})
+    .bundle()
+    .pipe(source('scripts.js'))
+    .pipe(buffer())
+    .pipe(plugins.babel({presets: ['es2015']}))
+    .pipe(gulp.dest('./dist/'))
+})
+
+
+gulp.task('dev-views', () => {
+  return gulp.src('./src/index.html')
+  .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('dev-browserSync', () => {return browserSync(
+  {
+    port: 8080,
+    server: {
+      baseDir: './',
+      middleware: [
+         modRewrite([
+           '(^[^\\.]*)$ /$1.html [L]'
+         ])
+      ],
+      debug: true
+    },
+    startPath: "dist/index.html"
+  }
+)})

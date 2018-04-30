4fc66dfcd5ef6f606307a9fcfb496f17790c5c4a
Jeewhan Kim <awryong@gmail.com>
2018-04-30 17:57:58 -0400

    boinga translations

--- src/js/parser.js
+++ src/js/parser.js
@@ -3,8 +3,9 @@ window.NYTD = {} // to Store Global Object name spaced as NYTD
 NYTD.render_section_front = (json) => {
 
   const $main = $('#nyt-main')
+  const $languageSelector = $('#languer-selector')
   /*
-  * Initial Validation
+  * Step 0. Initial Validation
   */
   if(json.page === undefined || json.page.content === undefined) {
     console.log('invalid JSON format')
@@ -33,43 +34,105 @@ NYTD.render_section_front = (json) => {
 
   /* 
   * Function: getImageUrl
-  * get thumbnail image URL from the images.types object
+  * get thumbnail image URL from the images.types object.
   */
   const getImageUrl = (images) => {
     let types = images[0].types.filter(t => t.type === "square320" || t.type === "wide")
     return types.length ? `http://www.nytimes.com/${types[0].content}` : null
   }
 
-  refinedContents.forEach((content, idx) => {
-    refinedData.push({
-      imageUrl: getImageUrl(content.assets[0].images),
-      figcaption: content.assets[0].typeOfMaterial,
-      link: content.assets[0].url,
-      headline: content.assets[0].headline,
-      summary: content.assets[0].summary,
-      publisher: content.assets[0].byline
+  /*
+  * Parse valid JSON data and stroe into the "refined" object.
+  */
+  const contentParser = (content, refinedContent) => {
+    content.forEach((content, idx) => {
+      refinedContent.push({
+        imageUrl: getImageUrl(content.assets[0].images),
+        figcaption: content.assets[0].typeOfMaterial,
+        link: content.assets[0].url,
+        headline: content.assets[0].headline,
+        summary: content.assets[0].summary,
+        publisher: content.assets[0].byline
+      })
     })
-  })
+  }
 
-  const viewController = (data) => {
+  function isUpperCase(aCharacter) {    
+    return (aCharacter >= 'A') && (aCharacter <= 'Z');
+  }
+
+  /*
+  * Function: Translate Maritian
+  */
+  const translate = (sentence, lang) => {
+    if(lang === 'en') return sentence
+    if(lang === 'ma') {
+      return sentence.split(' ').reduce((sentence, word) => {
+        if(word.length > 3) { // a. every word three characters or less is left alone
+          let returnWord = 'boinga' // b. every word more than three characters is replaced with boinga.
+          word.split('').forEach((letter, idx) => {
+            if(idx > returnWord.length) return
+            if(isUpperCase(letter)) {
+              // c. maintain the same capitalization and punctuation in the English and Martian Versions.
+              returnWord = returnWord.split('').map((letter, i) => {
+                if(idx === i) letter = letter.toUpperCase()
+                return letter
+              }).join('')
+            }
+          })
+          word = returnWord
+        }
+        return sentence + ' ' + word
+      }, '')
+    }
+  }
+
+  /*
+  * Generate & Render DOM HTML from refined data.
+  */
+  const viewController = (data, lang = 'en') => {
+    $main.html('')
     let html = ``
     html += `<section>`
     data.forEach((data, idx) => {
-      if(idx === 0 || (idx-3)%4 === 0) html += `<div class="nyt-row">` // create another row every 3 + 4n columns
+      if(idx === 0 || (idx-3)%4 === 0) {
+        html += `<div class="nyt-row">` // create another row every 3 + 4n columns
+      }
       html += `<div class="${idx === 0 ? 'nyt-col-2': ''}">` // only the first element should be 2 column-width
       html += `<a href="${data.link}"><picture><img src="${data.imageUrl}" tabindex="0"></picture></a>`
-      html += `<figcaption>${data.figcaption}</figcaption>`
-      html += `<a href="${data.link}"><h3>${data.headline}</h3></a>`
-      html += `<p>${data.summary}</p>`
+      html += `<figcaption>${translate(data.figcaption, lang)}</figcaption>`
+      html += `<a href="${data.link}"><h3>${translate(data.headline, lang)}</h3></a>`
+      html += `<p>${translate(data.summary, lang)}</p>`
       if(data.publisher !== undefined) {
-        html += `<span>${data.publisher}</span>`
+        html += `<span>${translate(data.publisher, lang)}</span>`
       }
       html += `</div>`
-      if(idx === 2 || (idx-2)%4 === 0) html += `</div>`
+      if(idx === 2 || (idx-2)%4 === 0) {
+        html += `</div>`
+      }
     })
     html += `</section>`
     $main.append(html) 
   }
 
+  /*
+  * Step 2.
+  */
+  contentParser(refinedContents, refinedData)
+  /*
+  * Step 3.
+  */
   viewController(refinedData)
+
+  $languageSelector.find('li').click(e => {
+    const option = $(e.currentTarget).attr('data-lang')
+    switch(option) {
+      case 'en':
+      viewController(refinedData)
+      break
+      case 'ma':
+      viewController(refinedData, 'ma')
+      break
+    }
+  })
 }

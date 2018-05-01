9cc7a9d343764678b9ec3eb02b576f3eb30d1c42
JeeWhan Kim <jeewhan.kim@michaelkors.com>
2018-04-30 20:48:29 -0400

    day 1

--- src/js/parser.js
+++ src/js/parser.js
@@ -95,7 +95,7 @@ NYTD.render_section_front = (json) => {
       if(idx === 0 || (idx-3)%4 === 0) {
         html += `<div class="nyt-row">` // create another row every 3 + 4n columns
       }
-      html += `<div class="${idx === 0 ? 'nyt-col-2': ''}">` // only the first element should be 2 column-width
+      html += `<div class="${idx === 0 ? 'nyt-col-2': 'nyt-col-1'}">` // only the first element should be 2 column-width
       html += `<a href="${data.link}"><div class="nyt-image"><img src="${data.imageUrl}" tabindex="0"></div></a>`
       html += `<figcaption>${translate(data.figcaption, lang)}</figcaption>`
       html += `<a href="${data.link}"><h3>${translate(data.headline, lang)}</h3></a>`
